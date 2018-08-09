663c6940d185e0990bdf6ab891068fac064fe713
Vincent Biret <vincentbiret@hotmail.com>
2018-08-08 20:43:17 -0400

    clearing useless remapers

--- config/karma.config.js
+++ config/karma.config.js
@@ -2,8 +2,6 @@
 var existingKarmaConfig = require('@microsoft/sp-build-web/lib/karma/karma.config');
 var _ = require('lodash');
 var junitReporter = require('karma-junit-reporter');
-var remapIstanbulReporter = require('karma-remap-istanbul');
-var remapCoverageReporter = require('karma-remap-coverage');
 
 module.exports = function (config) {
   existingKarmaConfig(config);
@@ -19,26 +17,15 @@ module.exports = function (config) {
     suite: 'karma', // suite will become the package name attribute in xml testsuite element
     useBrowserName: true, // add browser name to report and classes names
   };
-  var coverageReportsRoot = './coverage';
   var coberturaSubDir = 'cobertura';
   var coverageSubDir = 'lcov';
   var coberturaFileName = 'cobertura.xml';
   config.coverageReporter.reporters.push({type: 'cobertura', subdir: './' + coberturaSubDir, file: coberturaFileName});
-  config.remapIstanbulReporter = {
-    reports: {
-      cobertura: coverageReportsRoot + '/' + coberturaSubDir + '/' + coberturaFileName,
-    }
-  };
   config.coverageReporter.reporters.push({
     type: 'lcov',
     subdir: './' + coverageSubDir + '/',
     file: 'lcov.info'
   });
-  remapCoverageReporter = {
-    cobertura: coverageReportsRoot + '/' + coberturaSubDir + '/' + coberturaFileName,
-  };
   config.browserNoActivityTimeout = 60000;
-  config.plugins.push(remapIstanbulReporter);
-  config.plugins.push(remapCoverageReporter);
   config.plugins.push(junitReporter);
 };
